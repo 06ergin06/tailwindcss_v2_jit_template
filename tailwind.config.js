20513b9b256528bb9cb892f08902b6a4703379ea
Webtas <72016401+06ergin06@users.noreply.github.com>
2021-12-19 19:11:15 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
